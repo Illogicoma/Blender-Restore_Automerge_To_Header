eff6908ca1b4ab7db24935a17fd2b20011fd8394
Illogicoma <illogicoma@gmail.com>
2026-01-14 10:21:19 +0000

    Initial Commit

---
+++ Restore Automerge to Header/__init__.py
@@ -0,0 +1,23 @@
+# ------------------------------------------------------------------------------
+# Restore Auto Merge Button
+# Licensed under GNU General Public License v3 or later
+# ------------------------------------------------------------------------------
+import bpy
+
+def draw_automerge_header(self, context):
+    layout = self.layout
+    tool_settings = context.tool_settings
+    
+    if context.mode == 'EDIT_MESH':
+        row = layout.row(align=True)
+        row.separator(factor=0.5)
+        row.prop(tool_settings, "use_mesh_automerge", text="", icon='AUTOMERGE_ON')
+        
+        if tool_settings.use_mesh_automerge:
+            row.prop(tool_settings, "double_threshold", text="")
+
+def register():
+    bpy.types.VIEW3D_HT_header.append(draw_automerge_header)
+
+def unregister():
+    bpy.types.VIEW3D_HT_header.remove(draw_automerge_header)
